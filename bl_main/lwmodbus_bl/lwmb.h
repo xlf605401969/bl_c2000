430e3b5585f9718dd946518d7f30e6c95e1cefdb
Xiao Lifan <xiao605401969@live.com>
2026-01-03 11:45:52 +0800

    fix: 修改缓冲区大小，设置默认波特率为250k

--- bl_main/lwmodbus_bl/lwmb.h
+++ bl_main/lwmodbus_bl/lwmb.h
@@ -22,7 +22,7 @@
 #define LWMB_COMM_BAUDRATE 2000000L // 通信波特率
 
 #define LWMB_TX_MAX_LENGTH 64 // 最大发送长度
-#define LWMB_RX_MAX_LENGTH 128 // 最大接收长度
+#define LWMB_RX_MAX_LENGTH 256 // 最大接收长度
 // 内部状态定义
 typedef enum
 {
